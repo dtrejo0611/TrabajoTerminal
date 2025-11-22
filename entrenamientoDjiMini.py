15a11677c5d0f748600d2d18b2360797c9eafa5f
David <dtrejot021106@gmail.com>
2025-11-22 16:01:34 -0600

    entrenamiento falla en jetson

---
+++ entrenamientoDjiMini.py
@@ -0,0 +1,8 @@
+from ultralytics import YOLO
+
+if __name__ == "__main__":
+
+    modelo = YOLO("yolov8m.pt")
+    #Conforme realize los entrenamientos debo cambiar las epocas y el batch hasta que me de un resultado optimo
+    modelo.train(data="./datasets/djimini/data.yaml", epochs=20, batch=8, optimizer='Adam', lr0=0.0001, pretrained=True)
+
