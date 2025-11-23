74a85daee8fd8285be377a0aef8b16f0068b233c
David <dtrejot021106@gmail.com>
2025-11-22 19:00:36 -0600

    Entrenamiento realizado

--- entrenamientoDjiMini.py
+++ entrenamientoDjiMini.py
@@ -4,5 +4,5 @@ if __name__ == "__main__":
 
     modelo = YOLO("yolov8m.pt")
     #Conforme realize los entrenamientos debo cambiar las epocas y el batch hasta que me de un resultado optimo
-    modelo.train(data="./datasets/djimini/data.yaml", epochs=20, batch=8, optimizer='Adam', lr0=0.0001, pretrained=True)
-
+    modelo.train(data="./datasets/djimini/data.yaml", epochs=50, batch=8, 
+                 optimizer='Adam', lr0=0.0001, pretrained=True)
