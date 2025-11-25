3b82bd160f6ab656eab8bf64ffda5f4a0f27ccc6
David <dtrejot021106@gmail.com>
2025-11-24 22:02:38 -0600

    Reentrenamiento modelo small

--- entrenamientoDjiMini.py
+++ entrenamientoDjiMini.py
@@ -1,8 +1,20 @@
 from ultralytics import YOLO
 
 if __name__ == "__main__":
+    # Cargamos el modelo Small (buen balance para la Jetson Orin Nano)
+    modelo = YOLO("yolov8s.pt")
 
-    modelo = YOLO("yolov8m.pt")
-    #Conforme realize los entrenamientos debo cambiar las epocas y el batch hasta que me de un resultado optimo
-    modelo.train(data="./datasets/djimini/data.yaml", epochs=50, batch=8, 
-                 optimizer='Adam', lr0=0.0001, pretrained=True)
+    # Entrenamiento optimizado
+    modelo.train(
+        data="./datasets/djimini/data.yaml",
+        epochs=150,           # Subimos a 150, el Early Stopping lo parará si acaba antes
+        patience=30,          # Si no mejora en 30 épocas, se detiene solo
+        batch=16,             # 16 es seguro para la RTX 4050 con el modelo 's'
+        imgsz=640,            # Tamaño estándar. Si el drone es muy pequeño, podrías probar 960
+        device=0,             # Asegura que use tu GPU NVIDIA
+        workers=0,            # Núcleos de CPU para cargar datos
+        pretrained=True,      # Correcto
+        cache=True,           # ACELERA MUCHO: carga las 1000 fotos en RAM
+        project="Dron_Detection", # Nombre de la carpeta de salida para orden
+        name="entrenamiento_v1"
+    )
